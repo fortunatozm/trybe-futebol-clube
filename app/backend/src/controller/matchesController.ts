31fa0107514ff426c32af3da1b5e2f6c94b0c685
fortunatozm <fortunatompongo@hotmail.com>
2023-01-18 23:09:51 -0300

    requisitos 15 e 16 corrigidos

--- app/backend/src/controller/matchesController.ts
+++ app/backend/src/controller/matchesController.ts
@@ -1,33 +1,33 @@
 import { Request, Response } from 'express';
 import MatchesService from '../services/matchesService';
 
-class MatchController {
+class MatchesController {
   static async getAll(req: Request, res: Response) {
     const { inProgress } = req.query;
     if (inProgress !== undefined) {
-      const matches = await MatchService.getByProgress(inProgress === 'true');
+      const matches = await MatchesService.getByProgress(inProgress === 'true');
       return res.status(200).json(matches);
     }
-    const matches = await MatchService.getAll();
+    const matches = await MatchesService.getAll();
     res.status(200).json(matches);
   }
 
   static async finishMatch(req: Request, res: Response) {
     const { id } = req.params;
-    await MatchService.finishMatch(+id);
+    await MatchesService.finishMatch(+id);
     res.status(200).json({ message: 'Finished' });
   }
 
   static async createMatch(req: Request, res: Response) {
-    const match = await MatchService.createMatch(req.body);
+    const match = await MatchesService.createMatch(req.body);
     res.status(201).json(match);
   }
 
   static async updateMatch(req: Request, res: Response) {
     const { id } = req.params;
-    const match = await MatchService.updateMatch(+id, req.body);
+    const match = await MatchesService.updateMatch(+id, req.body);
     res.status(200).json(match);
   }
 }
 
-export default MatchController;
+export default MatchesController;
