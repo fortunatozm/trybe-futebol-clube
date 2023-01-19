a8142b9da3d4b920e1549a71bb445c2f5dcf838e
fortunatozm <fortunatompongo@hotmail.com>
2023-01-18 23:11:17 -0300

    requisitos 15 e 16 corrigidos2

--- app/backend/src/routes/myRoutes.ts
+++ app/backend/src/routes/myRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UsersController from '../controller/usersController';
 import TeamsController from '../controller/teamsController';
+import MatchesController from '../controller/matchesController';
 
 const router = Router();
 
@@ -8,6 +9,6 @@ router.post('/login', UsersController.login);
 router.get('/login/validate', UsersController.validate);
 router.get('/teams', TeamsController.getAll);
 router.get('/teams/:id', TeamsController.getOne);
-router.get('/matches', TeamsController.getOne);
+router.get('/matches', MatchesController.getAll);
 
 export default router;
